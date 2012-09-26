009ce382c6375583594a934141d807a274520211
Warner Godfrey <warner@ubuntu.(none)>
2012-09-26 05:11:33 -0700

    ubuntu 12.0.4 requires the interface to be claimed before sending any messages, also udev rules must be set so a non-privileged user can drive the device

--- lib/blinky/light.rb
+++ lib/blinky/light.rb
@@ -1,8 +1,15 @@
 module Blinky
   class Light
     
-    def initialize device_handle, recipe, plugins
-        @handle = device_handle
+    def initialize device, recipe, plugins
+        @handle = device.open
+        begin
+          @handle.usb_detach_kernel_driver_np(0)
+        rescue LIBUSB::ERROR_NOT_FOUND
+          # Already detached
+        end
+        @handle.set_configuration(device.configurations.first)
+	@handle.usb_claim_interface(0)
         self.extend(recipe)   
         plugins.each do |plugin|
             self.extend(plugin)
